af6507aa98971e4bb2c63899dd2e96f8d1b261a6
RedSquirrel <chapmanredsquirrel@outlook.com>
2023-02-23 18:49:39 -0500

    Fixed map walls.

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1677194096|62224218";
+const CACHE_VERSION = "1677196137|1086325521";
 const CACHE_PREFIX = "Retro Quiver-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
