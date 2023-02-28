aa2e4239b079dfe85819e6e13ed3619771c22cd5
RedSquirrel <chapmanredsquirrel@outlook.com>
2023-02-28 09:09:58 -0500

    GUI updates

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1677487992|2652473802";
+const CACHE_VERSION = "1677593359|1199257993";
 const CACHE_PREFIX = "Retro Quiver-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
